8f958b2c7019ca5d1f24967dfe388311d54cdd31
steveEECSrubin <konstantinrubin@engineer.com>
2019-06-11 09:39:44 -0400

    added caclulation of number of routing tables possible

--- dijkstra/code.js
+++ dijkstra/code.js
@@ -52,7 +52,7 @@ function display_grid(){
         data.push( {
           "data": {
             "id": "n-"+i+"-"+j,
-            "weight": j*COLS+i,
+            "weight": 0,
             // "label": "non"
           },
           "position": {
@@ -256,119 +256,116 @@ function display_grid(){
       // console.log(node_json);
       // console.log(node_json["data"]);
       console.log("deleting the node " + node.id());
-      var index_of_weight = node_json.indexOf("id")
-      var dead_node_id = parseInt(node_json.substring(index_of_weight+5, index_of_weight+13));
+      // var index_of_weight = node_json.indexOf("id")
+      // var dead_node_id = parseInt(node_json.substring(index_of_weight+5, index_of_weight+13));
       
       run_the_algorithm(parseInt(COLS), parseInt(ROWS), "#"+node.id());
     });
 
 
 
-    // calculate the weights using minimal path algorithm
-    
-    var i, j;
-//     for (i = 0; i < COLS; i++) {
-//       for (j = 0; j < ROWS; j++) {
-//         if(i==0 & j==0) continue; // skip the master
-
-//         var aStar = cy.elements().aStar({ root: "#n-0-0", goal: "#n-"+i+"-"+j, weight: function(edge){
-//   return edge.data('weight');
-// } });
-
-//         var minimum_hop_count;
-
-
-//         // checking the quater the node is in
-//         if ((i<=(COLS%2==0 ? Math.floor(COLS/2) : Math.floor(COLS/2))) && (j<=(ROWS%2==0 ? Math.floor(ROWS/2-1) : Math.floor(ROWS/2-1)))) //1st quater
-//         {
-//             minimum_hop_count = i+j;
-//           //    console.log("\n");
-//           // console.log(cy.$("#n-"+i+"-"+j).id());
-//           // console.log("minimum_hop_count - " + minimum_hop_count);
-
-//         }
-//         else if((i>=(COLS%2==0 ? Math.floor(COLS/2+1) : Math.floor(COLS/2+1))) && (j<=(ROWS%2==0 ? Math.floor(ROWS/2-1) : Math.floor(ROWS/2)))) 
-//         {
-//             minimum_hop_count = j+COLS-i;
-//           //    console.log("\n");
-//           // console.log(cy.$("#n-"+i+"-"+j).id());
-//           // console.log("minimum_hop_count - " + minimum_hop_count);
-
-//         }
-//         else if ((i>=(COLS%2==0 ? Math.floor(COLS/2) : Math.floor(COLS/2+1))) && (j>=(ROWS%2==0 ? Math.floor(ROWS/2) : Math.floor(ROWS/2+1))))
-//         {
-//             minimum_hop_count = ROWS-j+COLS-i;
-//           //    console.log("\n");
-//           // console.log(cy.$("#n-"+i+"-"+j).id());
-//           // console.log("minimum_hop_count - " + minimum_hop_count);
-
-//         }
-//         else if ((i<=(COLS%2==0 ? Math.floor(COLS/2-1) : Math.floor(COLS/2))) && (j>=(ROWS%2==0 ? Math.floor(ROWS/2) : Math.floor(ROWS/2))))
-//         {
-//             minimum_hop_count = ROWS-j+i;
-//           //    console.log("\n");
-//           // console.log(cy.$("#n-"+i+"-"+j).id());
-//           // console.log("minimum_hop_count - " + minimum_hop_count);
-
-//         }
-//         else 
-//           console.log("ERROR: THE QUATER FOR THE NODE WAS NOT DETECTED");
-
-
-         
-
-//         if ((aStar.path.length - 1)/2 > minimum_hop_count){ 
-//           console.log("\n");
-//           console.log("ERROR: THE MINIMAL PATH IS LONGER THAN MINIMIUM HOP COUNT");
-//           console.log("the minimum hop count is " + minimum_hop_count);
-//           console.log("but actual hop count " + (aStar.path.length - 1)/2);
-//           console.log("the distance (weight of the path) " + aStar.distance);
-//           // console.log(aStar.path);
-//         }
-//         if (!aStar.found) console.log("ERROR: MINUMUM PATH NOT FOUND");
-
-//             // console.log(aStar.path);
-//             var k;
-//             for ( k=0; k<aStar.path.length; k++ ) {  
-//                 if(aStar.path[k].isEdge()){ 
-//                     //console.log(aStar.path[k].id());
-//                     //chnage the weigth
-//                     // console.log(cy.$("#"+aStar.path[k].id()).data());
-//                     // console.log(cy.$("#"+aStar.path[k].id()).data().weight);
-//                     var current_weight = cy.$("#"+aStar.path[k].id()).data().weight;
-//                     current_weight++;
-//                     // cy.$("#"+aStar.path[k].id()).data.weight += 1;
-//                     cy.$("#"+aStar.path[k].id()).json({"data":{"weight":current_weight}});
-//                     cy.$("#"+aStar.path[k].id()).json({"data":{"label":current_weight}});
-//                     // console.log(cy.$("#"+aStar.path[k].id()).data());
-
-//                 }
-//             }
-
-//       }
-//     }
-    // console.log(cy.elements().length);
-    // var el = cy.remove('#n-9-9');
-    // console.log(cy.elements().length);
-    // el.restore();
-    // console.log(cy.elements().length);
-    // console.log(minimum_hop_count_between_nodes("#n-0-0", '#n-2-2'));
-
-
-    // for every node
-    var m,l,radius;
+    var i, j, radius;
+    var m,l;
     var kk, mm, ll, nn, ii;
 
 
-    for(radius = 1 ; radius <= COLS + ROWS; radius++) { // traverse in circles
-      // radius = 2;
-      kk = ll= ii =0;
-      mm = ROWS;
-      nn = COLS;
+    // determine how many possible routes we have if we select completely random routs but with min hop count
+    cy.$("#n-"+0+"-"+1).json({"data":{"weight":1}});
+    cy.$("#n-"+1+"-"+0).json({"data":{"weight":1}});
+    cy.$("#n-"+(COLS-1)+"-"+0).json({"data":{"weight":1}});
+    cy.$("#n-"+0+"-"+(ROWS-1)).json({"data":{"weight":1}});
+
+    var number_of_routs = 1;
+    for(radius = 1 ; radius <= COLS + ROWS; radius++) {
+      // for (j = 0; j < ROWS; j++) {
+      //   for (i = 0; i < COLS; i++) {
 
 
-    // for (j = 0; j < ROWS; j++) {
-    //   for (i = 0; i < COLS; i++) {
+        /*  kk - starting row index 
+        mm - ending row index 
+        ll - starting column index 
+        nn - ending column index 
+        ii - iterator 
+        */
+           
+
+        kk = ll = ii =0;
+        mm = ROWS;
+        nn = COLS;
+        // CLOCK WISE
+        while (kk < mm && ll < nn)
+        {
+            // Print the first row from the remaining rows 
+            for (ii = ll; ii < nn; ++ii) 
+            { 
+              // console.log("#n-"+kk+"-"+ii);
+              number_of_routs = number_of_routs * calculate_routes_from_node(ii, kk, radius, COLS, ROWS, dead_node);
+            } 
+            kk++; 
+   
+            // Print the last column from the remaining columns  
+            for (ii = kk; ii < mm; ++ii) 
+            { 
+              // console.log("#n-"+ii+"-"+(nn-1));
+              number_of_routs = number_of_routs * calculate_routes_from_node((nn-1), ii, radius, COLS, ROWS, dead_node);
+            } 
+            nn--; 
+   
+            // Print the last row from the remaining rows */ 
+            if ( kk < mm) 
+            { 
+                for (ii = nn-1; ii >= ll; --ii) 
+                { 
+                  // console.log("#n-"+(mm-1)+"-"+ii);
+                  number_of_routs = number_of_routs * calculate_routes_from_node(ii, (mm-1), radius, COLS, ROWS, dead_node);
+                } 
+                mm--; 
+            } 
+   
+            // Print the first column from the remaining columns */ 
+            if (ll < nn) 
+            { 
+                for (ii = mm-1; ii >= kk; --ii) 
+                { 
+                  // console.log("#n-"+ii+"-"+ll);
+                  number_of_routs = number_of_routs * calculate_routes_from_node(ll, ii, radius, COLS, ROWS, dead_node);
+                } 
+                ll++;     
+            }         
+        } 
+
+          
+      //   } 
+      // }
+    }
+
+
+    console.log("Number of the possible routes - " + number_of_routs);
+
+
+
+
+
+
+
+
+
+
+
+    // for(radius = 1 ; radius <= COLS + ROWS; radius++) { // traverse in circles
+    //   // radius = 2;
+    //   kk = ll= ii =0;
+    //   mm = ROWS;
+    //   nn = COLS;
+
+
+
+
+    for (j = 0; j < ROWS; j++) {
+      for (i = 0; i < COLS; i++) {
+
+        process_node(i, j, radius, COLS, ROWS, dead_node);
+      
 
 
         /*  kk - starting row index 
@@ -380,46 +377,46 @@ function display_grid(){
            
 
            // CLOCK WISE
-        while (kk < mm && ll < nn) 
-        { 
-            // Print the first row from the remaining rows 
-            for (ii = ll; ii < nn; ++ii) 
-            { 
-              // console.log("#n-"+kk+"-"+ii);
-              process_node(ii, kk, radius, COLS, ROWS, dead_node);
-            } 
-            kk++; 
+        // while (kk < mm && ll < nn) 
+        // {
+        //     // Print the first row from the remaining rows 
+        //     for (ii = ll; ii < nn; ++ii) 
+        //     { 
+        //       // console.log("#n-"+kk+"-"+ii);
+        //       process_node(ii, kk, radius, COLS, ROWS, dead_node);
+        //     } 
+        //     kk++; 
    
-            // Print the last column from the remaining columns  
-            for (ii = kk; ii < mm; ++ii) 
-            { 
-              // console.log("#n-"+ii+"-"+(nn-1));
-              process_node((nn-1), ii, radius, COLS, ROWS, dead_node);
-            } 
-            nn--; 
+        //     // Print the last column from the remaining columns  
+        //     for (ii = kk; ii < mm; ++ii) 
+        //     { 
+        //       // console.log("#n-"+ii+"-"+(nn-1));
+        //       process_node((nn-1), ii, radius, COLS, ROWS, dead_node);
+        //     } 
+        //     nn--; 
    
-            // Print the last row from the remaining rows */ 
-            if ( kk < mm) 
-            { 
-                for (ii = nn-1; ii >= ll; --ii) 
-                { 
-                  // console.log("#n-"+(mm-1)+"-"+ii);
-                  process_node(ii, (mm-1), radius, COLS, ROWS, dead_node);
-                } 
-                mm--; 
-            } 
+        //     // Print the last row from the remaining rows */ 
+        //     if ( kk < mm) 
+        //     { 
+        //         for (ii = nn-1; ii >= ll; --ii) 
+        //         { 
+        //           // console.log("#n-"+(mm-1)+"-"+ii);
+        //           process_node(ii, (mm-1), radius, COLS, ROWS, dead_node);
+        //         } 
+        //         mm--; 
+        //     } 
    
-            // Print the first column from the remaining columns */ 
-            if (ll < nn) 
-            { 
-                for (ii = mm-1; ii >= kk; --ii) 
-                { 
-                  // console.log("#n-"+ii+"-"+ll);
-                  process_node(ll, ii, radius, COLS, ROWS, dead_node);
-                } 
-                ll++;     
-            }         
-        } 
+        //     // Print the first column from the remaining columns */ 
+        //     if (ll < nn) 
+        //     { 
+        //         for (ii = mm-1; ii >= kk; --ii) 
+        //         { 
+        //           // console.log("#n-"+ii+"-"+ll);
+        //           process_node(ll, ii, radius, COLS, ROWS, dead_node);
+        //         } 
+        //         ll++;     
+        //     }         
+        // } 
 
         // COUNTER CLOCK WISE
         // while (kk < mm && ll < nn) 
@@ -464,15 +461,13 @@ function display_grid(){
         //     } 
         //     kk++; 
 
-        // } 
-
-        // process_node(i, j, radius, COLS, ROWS);
-
+          // process_node(i, j, radius, COLS, ROWS);
+      //   } 
       
-      }
-    // }
+      // }
+    }
 
-  // }
+  }
   console.log("\n");
   console.log("FINISHED PROCESSING THE NODES");
   console.log("\n");
@@ -482,6 +477,93 @@ function display_grid(){
 }
 }
 
+function calculate_routes_from_node(i, j, radius, COLS, ROWS, dead_node)
+{
+  var ret = 0;
+  var origin = "#n-0-0";
+  var destination = "#n-"+i+"-"+j;
+
+  if(destination == dead_node) return 1;
+  if(minimum_hop_count_between_nodes(origin, destination) != radius) return 1;
+  if (i == 0 && j == 0) return 1;
+  
+
+  // remove the nodes which are not in the min hop count
+  var removed_nodes = [];
+  for (m = 0; m < COLS; m++) {
+    for (l = 0; l < ROWS; l++) {
+    
+      if (m == 0 && l == 0) continue;
+      
+      var el = "#n-"+m+"-"+l;
+      if(el == dead_node) continue;
+      
+      if((!path_exists_between_nodes(origin, el) || !path_exists_between_nodes(el, destination) || !path_exists_between_nodes(origin, destination)) || (minimum_hop_count_between_nodes(origin, el) + minimum_hop_count_between_nodes(el, destination) > minimum_hop_count_between_nodes(origin, destination))) // the element is not in the subgraph of being in the shortest path between origin and the destination, then remove
+      {
+        //remove node outside of the subgraph
+        removed_nodes.push(cy.remove("#n-"+m+"-"+l));
+
+      }
+    }
+  }
+
+  // determin how many nodes ar connected to the current node (at most 4)
+  // find the indeces of the nodes which should be connected to the current node
+  var top_i, top_j, bot_i, bot_j, left_i, left_j, right_i, right_j;
+
+  // left node
+  if (i == 0) left_i = COLS - 1;
+  else left_i = i-1;
+
+  left_j = j;
+
+
+  // right node
+  if (i == COLS-1) right_i = 0;
+  else right_i = i+1;
+
+  right_j = j;
+
+  // node above
+  if (j == 0) top_j = ROWS - 1;
+  else top_j = j-1;
+
+  top_i = i;
+
+  // node below
+  if (j == ROWS - 1) bot_j = 0;
+  else bot_j = j+1;
+
+  bot_i = i;
+
+
+  // if node below exists
+  if (cy.$("#n-"+bot_i+"-"+bot_j).inside()) ret = ret + cy.$("#n-"+bot_i+"-"+bot_j).data('weight');
+
+  // if node above exists
+  if (cy.$("#n-"+top_i+"-"+top_j).inside()) ret = ret + cy.$("#n-"+top_i+"-"+top_j).data('weight');
+
+  // if node left exists
+  if (cy.$("#n-"+left_i+"-"+left_j).inside()) ret = ret + cy.$("#n-"+left_i+"-"+left_j).data('weight');
+
+  // if node right exists
+  if (cy.$("#n-"+right_i+"-"+right_j).inside()) ret = ret + cy.$("#n-"+right_i+"-"+right_j).data('weight');
+
+  // plus the current weight which is 0 unless the node is right next to the master
+  ret = ret + cy.$("#n-"+i+"-"+j).data('weight');
+
+
+  // restore all the deleted elements
+  for (k = removed_nodes.length-1; k >= 0; k--) { 
+    removed_nodes[k].restore();
+  }
+
+  //assign the number of paths to this node
+  cy.$("#n-"+i+"-"+j).json({"data":{"weight":ret}});
+
+  return ret;
+
+}
 
 function process_node(i, j, radius, COLS, ROWS, dead_node)
 {
@@ -492,7 +574,7 @@ function process_node(i, j, radius, COLS, ROWS, dead_node)
       if(destination == dead_node) return;
       // console.log(destination);
       // console.log(dead_node);
-      if(minimum_hop_count_between_nodes(origin, destination) != radius) return;
+      // if(minimum_hop_count_between_nodes(origin, destination) != radius) return;
       
       
         // i = 7;
