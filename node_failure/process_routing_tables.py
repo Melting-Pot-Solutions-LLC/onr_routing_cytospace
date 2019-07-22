c71d94323409358e91cc67b4538b8c4e175b2bda
steveEECSrubin <konstantinrubin@engineer.com>
2019-07-22 11:42:10 -0400

    started writing a pythong script reading the array of paths

---
+++ node_failure/process_routing_tables.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+"""
+DESCRIPTION GOES HERE
+"""
+
+import sys
+import os
+import traceback
+import argparse
+import time
+import logging
+
+file = open("save.txt","r+")
+print("PROCESSING ROUTING TABLES")
+print(file.read().splitlines())
+# for line in file:
+	# print(line)
+
+
+
+
+
+
+file.close()
